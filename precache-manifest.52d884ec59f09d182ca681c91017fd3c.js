ac73c19373827cb696c68f087f2e6f3d61630552
Rhys Almario <rhysalmario@Rhyss-MacBook-Pro.local>
2020-10-19 19:04:38 -0700

    Updates

---
+++ precache-manifest.52d884ec59f09d182ca681c91017fd3c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be3b83a9bb26b5d53906bdad23790181",
+    "url": "/pokepack/index.html"
+  },
+  {
+    "revision": "be64ac9205c580a2d095",
+    "url": "/pokepack/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "b4bbbb41e34480fffb7b",
+    "url": "/pokepack/static/css/main.fb705401.chunk.css"
+  },
+  {
+    "revision": "be64ac9205c580a2d095",
+    "url": "/pokepack/static/js/2.b0a116c9.chunk.js"
+  },
+  {
+    "revision": "55f390cdd1a805c6a533111998b6fb4c",
+    "url": "/pokepack/static/js/2.b0a116c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4bbbb41e34480fffb7b",
+    "url": "/pokepack/static/js/main.b33875ea.chunk.js"
+  },
+  {
+    "revision": "d0d8dc39d5a4c5302379",
+    "url": "/pokepack/static/js/runtime-main.a58e651f.js"
+  },
+  {
+    "revision": "36d26dbcd032a9d91d891c9f22dfdec6",
+    "url": "/pokepack/static/media/Pokemon Solid.36d26dbc.ttf"
+  }
+]);
