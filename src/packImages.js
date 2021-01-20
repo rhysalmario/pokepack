99701c865a9d0ead1ae03e0e128c4656ee13f6e0
Rhys Almario <rhysa52@gmail.com>
2021-01-20 15:06:55 -0800

    updated pack images

--- src/packImages.js
+++ src/packImages.js
@@ -1,5 +1,5 @@
 export const packImages = [
-  'https://cdn.bulbagarden.net/upload/6/61/Set_Base_Charizard_Shadowless.jpg',
-  'https://cdn.bulbagarden.net/upload/0/0b/Set_Base_Blastoise_Shadowless.jpg',
-  'https://cdn.bulbagarden.net/upload/4/43/Set_Base_Venusaur_Shadowless.jpg'
+  'https://d1rw89lz12ur5s.cloudfront.net/photo/collectorscache/file/768c3720555211e89a532318b466b769/1st%20charizard%20pack.jpg',
+  'https://d1rw89lz12ur5s.cloudfront.net/photo/collectorscache/file/2680d750555111e8942bfd5bfeaf0da0/shadowless%20blastoise.jpg',
+  'https://d1rw89lz12ur5s.cloudfront.net/photo/collectorscache/file/80669ac0555111e8ae630903bce07812/shadowless%20venusaur.jpg'
 ];
